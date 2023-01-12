79137123ddef42beb4959544350f3d510a15a0a7
edwin <ngisahedwin@gmail.com>
2023-01-12 15:33:28 +0300

    added header section content

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    <div>
-        Header
+    <div className='gpt__header section__padding' id='home'>
+      <div className='gpt__header-content'>
+        <h1 className='gradient__text'>Let’s Build Something amazing with GPT-3 OpenAI</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+
+        <div className='gpt3__header-content_input'>
+          <input type='email' placeholder='type your email address' />
+          <button type='button'>Get started</button>
+        </div>
+        <div
+        className='gpt3__header-content__people'>
+          <img src={people}/>
+        </div>
+      </div>
         </div>
   )
 }
