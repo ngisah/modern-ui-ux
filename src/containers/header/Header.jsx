87e48f8e7f27311c3e3b496b4ecb4e40ec95c986
edwin <ngisahedwin@gmail.com>
2023-01-12 15:46:10 +0300

    header styling

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -16,7 +16,10 @@ const Header = () => {
         </div>
         <div
         className='gpt3__header-content__people'>
-          <img src={people}/>
+          <img src={people} alt='people' />
+        </div>
+        <div className='gpt3__header-image'>
+          <img src={ai} alt='ai' />
         </div>
       </div>
         </div>
