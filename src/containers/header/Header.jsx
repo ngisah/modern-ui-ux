37f51d8f644288ce7ac6f6f06996ccbcf95c9332
edwin <ngisahedwin@gmail.com>
2023-01-12 16:41:35 +0300

    styling

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -11,16 +11,17 @@ const Header = () => {
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
         <div className='gpt3__header-content_input'>
-          <input type='email' placeholder='type your email address' />
+          <input type='email' placeholder='Your email address' />
           <button type='button'>Get started</button>
         </div>
         <div
         className='gpt3__header-content__people'>
           <img src={people} alt='people' />
+          <p>1,600 people requested access a visit in last 24 hours</p>
         </div>
-        <div className='gpt3__header-image'>
+        {/* <div className='gpt3__header-image'>
           <img src={ai} alt='ai' />
-        </div>
+        </div> */}
       </div>
         </div>
   )
