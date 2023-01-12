6673bfffdd957e7f8b5ab963eb9870adb562a274
edwin <ngisahedwin@gmail.com>
2023-01-12 12:18:45 +0300

    styling

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -11,12 +11,16 @@ const Navbar= () => {
                 <img src={logo} alt='logo' />
 
             </div>
-            <div gpt3__navbar-links_container>
+            <div className='gpt3__navbar-links_container'>
                 <p><a href='#home'>Home</a></p>
                 <p><a href='#wgpt3'>What is GPT3?</a></p>
                 <p><a href='#possibilities'>Open AI</a></p>
                 <p><a href='#features'>Case Studies</a></p>
                 <p><a href='#blog'>Library</a></p>
+            </div>
+            <div className='gpt3__navbar-sign'>
+              <p>Sign In</p>
+              <button type='button'>Sign up</button>
             </div>
 
         </div>
